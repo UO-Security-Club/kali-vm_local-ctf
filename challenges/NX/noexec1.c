1f5c7d2e0171b1fef53a7d0bf6fe5b83ce9d24b8
root <root@ip-172-31-13-71.us-west-2.compute.internal>
2017-08-24 17:34:25 +0000

    added noexec1

--- challenges/NX/noexec1.c
+++ challenges/NX/noexec1.c
@@ -1,10 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 void func1() {
 	char buf[64];
+
+	/*Zero-out memory contents of buf*/
+	memset(buf, 0, 64);
+	puts("Enter User Data:");
+
+	/*Read up to 76 bytes of data from stdin (int fd 0) into buf*/
+	read(0, buf, 76);
+
+	printf("Data entered: %s", buf);
+	return;
 }
 
 void main() {
+
+	func1();
 	return;
 }
---
+++ res/compile_NX1.sh
@@ -0,0 +1,1 @@
+gcc -m32 -mpreferred-stack-boundary=2 noexec1.c -o noexec1 -fno-stack-protector
